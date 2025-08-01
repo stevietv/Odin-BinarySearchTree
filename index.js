9d31a7fbe7b327f3284f619ff7e0b8f18fade269
Stevie Robinson <stevie.robinson@gmail.com>
2025-08-01 12:06:50 +0200

    add tests driver for RandomTree as per requirements

--- index.js
+++ index.js
@@ -18,42 +18,89 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 
 prettyPrint(tree.root);
 
-console.log("-----------------------------------------")
+console.log("-------- Insert two values 99 + 9999 --------")
 
 tree.insert(99);
-prettyPrint(tree.root);
-
-console.log("-----------------------------------------")
 tree.insert(9999);
 prettyPrint(tree.root);
 
-console.log("-------------------------------------------------")
+console.log("-------- Delete 67 and 8 --------")
 tree.delete(67);
-prettyPrint(tree.root);
-console.log("-------------------------------------------------")
 tree.delete(8);
 prettyPrint(tree.root);
 
+console.log("-------- Find node 9 --------")
 console.log(tree.find(9));
 
-tree.levelOrderForEach((node) => { console.log(node.data) });
+console.log("-------- log breadth first level order --------")
+let arr = []
+tree.levelOrderForEach((node) => { arr.push(node.data) });
+console.log([...arr]);
 
-console.log("-------------------------------------------------")
-tree.preorder((node) => { console.log(node.data) });
+console.log("-------- log depth first preorder --------")
+arr = [];
+tree.preorder((node) => { arr.push(node.data) });
+console.log([...arr]);
 
-console.log("-------------------------------------------------")
-tree.inorder((node) => { console.log(node.data) });
+console.log("-------- log depth first inorder --------")
+arr = [];
+tree.inorder((node) => { arr.push(node.data) });
+console.log([...arr]);
 
-console.log("-------------------------------------------------")
-tree.postorder((node) => { console.log(node.data) });
 
-console.log("-------------------------------------------------")
+console.log("-------- log depth first postorder --------")
+arr = [];
+tree.postorder((node) => { arr.push(node.data) });
+console.log([...arr]);
+
+
+console.log("-------- log depth of 6345 --------")
 console.log(tree.depth(6345));
 
-console.log("-------------------------------------------------")
+console.log("-------- log height of 99 --------")
 console.log(tree.height(99));
 
-console.log("-----------------------------------------")
+console.log("-------- check if balanced, rebalance and check again --------")
 console.log(tree.isBalanced());
 tree.rebalance();
 console.log(tree.isBalanced());
+
+console.log("")
+console.log("-------- REQUIRED CHECKS WITH RANDOM TREE --------")
+console.log("")
+
+let randomTree = new Tree(Array.from({length: 20}, () => Math.floor(Math.random() * 100)));
+prettyPrint(randomTree.root);
+console.log(`The randomTree is balanced: ${randomTree.isBalanced()}`);
+let levelOrder = "";
+let preOrder = "";
+let postOrder = "";
+let inOrder = "";
+randomTree.levelOrderForEach((node) => { levelOrder += `${node.data}, ` });
+randomTree.preorder((node) => { preOrder += `${node.data}, ` });
+randomTree.postorder((node) => { postOrder += `${node.data}, ` });
+randomTree.inorder((node) => { inOrder += `${node.data}, ` });
+console.log(`levelOrder = ${levelOrder}`);
+console.log(`preOrder = ${preOrder}`);
+console.log(`postOrder = ${postOrder}`);
+console.log(`inOrder = ${inOrder}`);
+randomTree.insert(150);
+randomTree.insert(175);
+randomTree.insert(200);
+randomTree.insert(275);
+console.log(`The randomTree is balanced: ${randomTree.isBalanced()}`);
+randomTree.rebalance();
+console.log(`The randomTree is balanced: ${randomTree.isBalanced()}`);
+levelOrder = "";
+preOrder = "";
+postOrder = "";
+inOrder = "";
+randomTree.levelOrderForEach((node) => { levelOrder += `${node.data}, ` });
+randomTree.preorder((node) => { preOrder += `${node.data}, ` });
+randomTree.postorder((node) => { postOrder += `${node.data}, ` });
+randomTree.inorder((node) => { inOrder += `${node.data}, ` });
+console.log(`levelOrder = ${levelOrder}`);
+console.log(`preOrder = ${preOrder}`);
+console.log(`postOrder = ${postOrder}`);
+console.log(`inOrder = ${inOrder}`);
+
